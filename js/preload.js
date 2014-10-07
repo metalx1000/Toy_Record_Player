25cef614127d03db181aba812bff59d01b859f40
Kris Occhipinti <metal_bullets@filmsbykris.com>
2014-10-06 21:02:43 -0400

    song plays on record click

--- js/preload.js
+++ js/preload.js
@@ -28,10 +28,12 @@ preload.prototype = {
        	        this.game.load.image(images[i],"res/" + images[i] + ".png");
             }
 
-            records = ["little_star","doggie"]
+            records = ["little_star","doggie"];
+            song = [];
             for(var i = 0; i < records.length;i++){
        	        this.game.load.image(records[i]+"_img","res/records/" + records[i] + "/record.png");
-                this.game.load.audio(records[i]+"_music", ['res/records/' + records[i] + '/music.mp3','res/records/' + records[i] + '/music.ogg'])
+                this.game.load.audio(records[i]+"_music", ['res/records/' + records[i] + '/music.mp3','res/records/' + records[i] + '/music.ogg']);
+                song.push(this.game.add.audio(records[i]+"_music"));
             }
 
 	},
--- js/thegame.js
+++ js/thegame.js
@@ -16,11 +16,13 @@ theGame.prototype = {
         load_records: function(){
             for(var i = 0 ;i<records.length;i++){
                 var record = this.game.add.button(0,128 * i + 256,records[i] + "_img",this.play,this);
+                record.song = song[i];
                 record.anchor.setTo(0.5,0.5);
             }
         },
 
         play: function(record){
+            record.song.play();
             tween = this.game.add.tween(record).to({ x: this.game.world.width * 0.5, y: this.game.world.height * 0.5 }, 1000);
             tween.start();
         }
