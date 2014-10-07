ea9872c2c67349a3c7ac300f301fb757256e1b84
Kris Occhipinti <metal_bullets@filmsbykris.com>
2014-10-07 08:19:15 -0400

    Added record size tween and music loops now

--- js/thegame.js
+++ js/thegame.js
@@ -5,7 +5,8 @@ theGame.prototype = {
   	create: function(){
             var menu = this.game.add.button(this.game.world.width * 0.9,this.game.world.height * 0.1,"menu",this.menu,this);
 	    menu.anchor.setTo(0.5,0.5);
-        
+ 
+            rec_scale = 0.7;       
             this.load_records();
 	},
         update: function(){
@@ -26,6 +27,7 @@ theGame.prototype = {
             rec = [];
             for(var i = 0 ;i<records.length;i++){
                 rec.push(this.game.add.button(0,128 * i + 256,records[i] + "_img",this.play,this));
+                rec[i].scale.setTo(rec_scale,rec_scale);
                 rec[i].active = false;
                 rec[i].song = song[i];
                 rec[i].anchor.setTo(0.5,0.5);
@@ -35,16 +37,20 @@ theGame.prototype = {
         play: function(record){
             this.stop();
             record.active = true;
-            record.song.play();
-            tween = this.game.add.tween(record).to({ x: this.game.world.width * 0.5, y: this.game.world.height * 0.5 }, 1000);
-            tween.start();
+            record.song.play('',0,1,true);
+            tweenp = this.game.add.tween(record).to({ x: this.game.world.width * 0.5, y: this.game.world.height * 0.5 }, 1000);
+            tweens = this.game.add.tween(record.scale).to({ x: 1.5, y: 1.5 }, 1000);
+            tweenp.start();
+            tweens.start();
         },
 
         stop: function(){
             for(var i = 0 ;i<records.length;i++){
                 rec[i].active = false;
-                tween = this.game.add.tween(rec[i]).to({ x: 0, y: 128 * i + 256 }, 1000);
-                tween.start();
+                tweenp = this.game.add.tween(rec[i]).to({ x: 0, y: 128 * i + 256 }, 1000);
+                tweens = this.game.add.tween(rec[i].scale).to({ x: rec_scale, y: rec_scale }, 1000);
+                tweens.start();
+                tweenp.start();
                 song[i].stop();
             }
         }
